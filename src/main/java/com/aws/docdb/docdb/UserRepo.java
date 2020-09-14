7919f4937d3959039ba871617d90e9e0ef36182d
Li <ljunje@f018982d13cc.ant.amazon.com>
2020-09-14 23:03:45 +0800

    native

--- src/main/java/com/aws/docdb/docdb/UserRepo.java
+++ src/main/java/com/aws/docdb/docdb/UserRepo.java
@@ -2,7 +2,6 @@ package com.aws.docdb.docdb;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -10,7 +9,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.awt.print.Pageable;
 import java.util.List;
 
-public interface UserRepo extends JpaRepository<Users, String> {
+public interface UserRepo extends PagingAndSortingRepository<Users, String> {
 
     @Query("{name : {$regex : ?0}}")
     Page<Users> findByNickNameLike(String nickName, PageRequest pageable);
